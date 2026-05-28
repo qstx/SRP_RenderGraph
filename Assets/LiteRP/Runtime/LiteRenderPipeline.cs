ca4367bdf54ba34fc25df4687f2b00e5ead520eb
qstx <2031074519@qq.com>
2026-05-28 23:53:36 +0800

    第二节 渲染管线的基础流程，主要讲解RenderPipeline中的一些基础渲染流程和渲染回调事件

--- Assets/LiteRP/Runtime/LiteRenderPipeline.cs
+++ Assets/LiteRP/Runtime/LiteRenderPipeline.cs
@@ -8,7 +8,34 @@ namespace LiteRP
     {
         protected override void Render(ScriptableRenderContext context, List<Camera> cameras)
         {
-            base.Render(context, cameras);
+            BeginContextRendering(context, cameras);
+            for (int i = 0; i < cameras.Count; i++)
+            {
+                Camera camera = cameras[i];
+                RenderCamera(context, camera);
+            }
+            EndContextRendering(context, cameras);
+        }
+
+        private void RenderCamera(ScriptableRenderContext context, Camera camera)
+        {
+            BeginCameraRendering(context, camera);
+            
+            ScriptableCullingParameters cullingParameters = new ScriptableCullingParameters();
+            if(!camera.TryGetCullingParameters(out cullingParameters))
+                return;
+            CullingResults cullingResults = context.Cull(ref cullingParameters);
+            
+            CommandBuffer cmd = CommandBufferPool.Get(camera.name);
+            context.SetupCameraProperties(camera);
+            
+            context.ExecuteCommandBuffer(cmd);
+            cmd.Clear();
+            CommandBufferPool.Release(cmd);
+            
+            context.Submit();
+            
+            EndCameraRendering(context, camera);
         }
     }
 }
