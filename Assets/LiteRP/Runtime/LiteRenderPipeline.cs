7bf9ca22d33125b4c4e491f4500e14065c4f483b
qstx <2031074519@qq.com>
2026-05-29 08:39:05 +0800

    第三节 可见对象的渲染，主要讲解不透明、透明、半透明物体在自定义渲染管线中如何渲染，也包括天空盒与相机Clear相关实现

--- Assets/LiteRP/Runtime/LiteRenderPipeline.cs
+++ Assets/LiteRP/Runtime/LiteRenderPipeline.cs
@@ -6,6 +6,7 @@ namespace LiteRP
 {
     public class LiteRenderPipeline : RenderPipeline
     {
+        private readonly static ShaderTagId s_ShaderTagId = new ShaderTagId("SRPDefaultUnlit");
         protected override void Render(ScriptableRenderContext context, List<Camera> cameras)
         {
             BeginContextRendering(context, cameras);
@@ -28,6 +29,30 @@ namespace LiteRP
             
             CommandBuffer cmd = CommandBufferPool.Get(camera.name);
             context.SetupCameraProperties(camera);
+            
+            bool clearSkybox = camera.clearFlags == CameraClearFlags.Skybox;
+            bool clearDepth = camera.depth == 0;
+            bool clearColor=camera.clearFlags == CameraClearFlags.Color;
+            cmd.ClearRenderTarget(true,true, CoreUtils.ConvertSRGBToActiveColorSpace(camera.backgroundColor));
+
+            if (clearSkybox)
+            {
+                var skyboxRenderer = context.CreateSkyboxRendererList(camera);
+                cmd.DrawRendererList(skyboxRenderer);
+            }
+            
+            var sortingSettings = new SortingSettings(camera);
+            var drawSettings = new DrawingSettings(s_ShaderTagId,sortingSettings);
+            var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+            var rendererListParams = new RendererListParams(cullingResults, drawSettings, filteringSettings);
+            var rendererList = context.CreateRendererList(ref rendererListParams); 
+            cmd.DrawRendererList(rendererList);
+
+            sortingSettings.criteria = SortingCriteria.CommonTransparent;
+            filteringSettings = new FilteringSettings(RenderQueueRange.transparent);
+            rendererListParams = new RendererListParams(cullingResults, drawSettings, filteringSettings);
+            rendererList = context.CreateRendererList(ref rendererListParams); 
+            cmd.DrawRendererList(rendererList);
             
             context.ExecuteCommandBuffer(cmd);
             cmd.Clear();
