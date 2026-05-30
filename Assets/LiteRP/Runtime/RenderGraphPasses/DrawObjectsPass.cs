990e68ba460354458c399c0665e2dd46b3c74f87
qstx <2031074519@qq.com>
2026-05-30 10:05:39 +0800

    第五节 RenderGraphPass基础，主要讲解RenderGraphPass执行过程、分类以及给RenderGraph添加Pass的基础流程。

---
+++ Assets/LiteRP/Runtime/RenderGraphPasses/DrawObjectsPass.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.RenderGraphModule;
+
+namespace LiteRP.RenderGraphPasses
+{
+    public partial class LiteRenderGraphRecorder
+    {
+        private readonly static ProfilingSampler s_DrawObjectsProfilingSimplers=new ProfilingSampler("Draw Objects");
+        internal class DrawObjectsPassData
+        {
+            
+        }
+
+        private void AddDrawObjectsPass(RenderGraph renderGraph, ContextContainer frameData)
+        {
+            using (var builder = renderGraph.AddRasterRenderPass<DrawObjectsPassData>("Draw Objects Pass", out var passData, s_DrawObjectsProfilingSimplers))
+            {
+                builder.SetRenderFunc((DrawObjectsPassData passData, RasterGraphContext context) =>
+                {
+                    
+                });
+            }
+        }
+    }
+    
+}
